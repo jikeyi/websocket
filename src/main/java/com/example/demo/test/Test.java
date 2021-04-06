ea19a61f6d31224bdfd494064f69dce49b5ea6c1
jikeyi <60998476+jikeyi@users.noreply.github.com>
2021-04-06 13:51:57 +0800

    Update Test.java

--- src/main/java/com/example/demo/test/Test.java
+++ src/main/java/com/example/demo/test/Test.java
@@ -3,5 +3,6 @@ package com.example.demo.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("svn test");
+        System.out.println("哇哈哈");
     }
 }
