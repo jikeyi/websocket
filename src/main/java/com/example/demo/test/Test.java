6e2ac4c248f897b4eae65cdd993aea7538c88dee
tom <1933209693@qq.com>
2021-07-31 08:06:05 +0800

    修改test类

--- src/main/java/com/example/demo/test/Test.java
+++ src/main/java/com/example/demo/test/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("svn test");
         System.out.println("哇哈哈");
-        System.out.println("你好吗");
+        System.out.println("你好吗1111111111111111111");
     }
 }
