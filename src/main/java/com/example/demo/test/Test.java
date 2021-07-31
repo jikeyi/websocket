ce80590170f3fbb83cfde62ef425d9eb4f60e3ff
tom <1933209693@qq.com>
2021-07-31 08:21:16 +0800

    修改再次test

--- src/main/java/com/example/demo/test/Test.java
+++ src/main/java/com/example/demo/test/Test.java
@@ -3,7 +3,7 @@ package com.example.demo.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("svn test");
-        System.out.println("哇哈哈");
+        System.out.println("哇哈哈你经常拿出");
         System.out.println("你好吗1111111111111111111");
     }
 }
